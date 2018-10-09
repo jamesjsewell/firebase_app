22bdb1381c668e62e797b9d1a34aab7e3699f968
james sewell <jamesjosephsewell@gmail.com>
2018-10-09 18:19:45 -0500

    adjusted navbar presentation

--- public/navbar.js
+++ public/navbar.js
@@ -2,38 +2,37 @@
 function navbarRender (onSelect, loggedIn) {
   $('#navbar').html(`
 
-    <button class="${selectedCategory === 'misc' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1" path="misc">misc</button>
-    <button class="${selectedCategory === 'terminal' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1" path="terminal">terminal</button>
-    <button class="${selectedCategory === 'git' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1"  path="git">git</button>
-    <button class="${selectedCategory === 'editors' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1"  path="editors">editors</button>
-    <button class="${selectedCategory === 'python' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1"  path="python">python</button>
-    <button class="${selectedCategory === 'javascript' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1"  path="javascript">javascript</button>
-    <button class="${selectedCategory === 'html' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn m-1"  path="html">html</button>
     <div class="btn-group">
-      <button class="${selectedCategory.includes('css') ? 'btn-secondary ' : 'btn-outline-primary '} btn m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" path="none">css</button>
+      <button class="${selectedCategory.includes('css') ? 'active btn-primary ' : 'btn-outline-primary '} btn m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" path="none">css</button>
       <div class="dropdown-menu">
-        <div class="list-group">
-          <button class="${selectedCategory === 'css/general' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/general" >general</button>
-          <button class="${selectedCategory === 'css/frameworks' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/frameworks" >frameworks</button>
-          <button class="${selectedCategory === 'css/icons' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1"  path="css/icons">icons</button>
-          <button class="${selectedCategory === 'css/fonts' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/fonts">fonts</button>
-          <button class="${selectedCategory === 'css/gradients' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/gradients" >gradients</button>
-          <button class="${selectedCategory === 'css/svgs' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/svgs">svgs</button>
-          <button class="${selectedCategory === 'css/effects' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="css/effects">effects</button>
-        </div>
+          <button class="${selectedCategory === 'css/general' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/general" >general</button>
+          <button class="${selectedCategory === 'css/frameworks' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/frameworks" >frameworks</button>
+          <button class="${selectedCategory === 'css/icons' ? 'active' : ''} category_link dropdown-item btn-sm m-1"  path="css/icons">icons</button>
+          <button class="${selectedCategory === 'css/fonts' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/fonts">fonts</button>
+          <button class="${selectedCategory === 'css/gradients' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/gradients" >gradients</button>
+          <button class="${selectedCategory === 'css/svgs' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/svgs">svgs</button>
+          <button class="${selectedCategory === 'css/effects' ? 'active' : ''} category_link dropdown-item btn-sm m-1" path="css/effects">effects</button>
       </div>
     </div>
+    <button class="${selectedCategory === 'javascript' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1"  path="javascript">javascript</button>
+    <button class="${selectedCategory === 'html' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1"  path="html">html</button>
+    <div class="btn-group">
+      <button class="${selectedCategory.includes('backend') ? 'active btn-primary ' : 'btn-outline-primary '} btn m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" path="none">backend</button>
+      <div class="dropdown-menu">
+  
+        <button class="${selectedCategory === 'backend/mongodb' ? 'active btn-primary ' : 'btn-outline-primary '} category_link dropdown-item btn-sm m-1" path="backend/mongodb" >mongodb</button>
+        <button class="${selectedCategory === 'backend/postgres' ? 'active btn-primary ' : 'btn-outline-primary '} category_link dropdown-item btn-sm m-1" path="backend/postgres" >postgres</button>
+        <button class="${selectedCategory === 'backend/sql' ? 'active btn-primary ' : 'btn-outline-primary '} category_link dropdown-item btn-sm m-1"  path="backend/sql">sql</button>
+     
+      </div>
+    </div>
+    <button class="${selectedCategory === 'python' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1"  path="python">python</button>
+    <button class="${selectedCategory === 'terminal' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1" path="terminal">terminal</button>
+    <button class="${selectedCategory === 'git' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1"  path="git">git</button>
+    <button class="${selectedCategory === 'editors' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1"  path="editors">editors</button>
+    <button class="${selectedCategory === 'misc' ? 'active btn-primary ' : 'btn-outline-primary '} category_link btn m-1" path="misc">misc</button>
+    
 
-    <div class="btn-group">
-      <button class="${selectedCategory.includes('backend') ? 'btn-secondary ' : 'btn-outline-primary '} btn m-1 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" path="none">backend</button>
-      <div class="dropdown-menu">
-        <div class="list-group">
-          <button class="${selectedCategory === 'backend/mongodb' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="backend/mongodb" >mongodb</button>
-          <button class="${selectedCategory === 'backend/postgres' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1" path="backend/postgres" >postgres</button>
-          <button class="${selectedCategory === 'backend/sql' ? 'btn-secondary ' : 'btn-outline-primary '} category_link btn-sm m-1"  path="backend/sql">sql</button>
-        </div>
-      </div>
-    </div>
 
     ${loggedIn ? `<button id='logout_btn' class='btn-warning m-1'>logout</button>` : ''}
     
